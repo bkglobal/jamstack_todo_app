0ad63520fe82502c040eebd6ec5be81fd7314ec3
Bilal khatri <muhammadbilal@LAPTOP-F4917TNU.localdomain>
2021-05-21 19:02:29 +0500

    modified

--- app/www/src/pages/index.js
+++ app/www/src/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import medBannerImage from '../../assets/medBannerImg.png';
 
 export default function MainPage(props) {
     return (
-        <div>Hellow world</div>
+        <div>
+            <div>Hellow There..</div>
+            <div>Here are some images listed below</div>
+            <img src={medBannerImage} />
+        </div>
     ); 
 } 
