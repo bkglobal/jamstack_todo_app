06affc976a3c68308b0c0b5075eccaa43991ce4c
Bilal khatri <muhammadbilal@LAPTOP-F4917TNU.localdomain>
2021-05-27 16:52:18 +0500

    Removed image

--- app/www/src/pages/index.js
+++ app/www/src/pages/index.js
@@ -6,7 +6,6 @@ export default function MainPage(props) {
         <div>
             <div>Hellow There..</div>
             <div>Here are some images listed below</div>
-            <img src={medBannerImage} />
         </div>
     ); 
 } 
